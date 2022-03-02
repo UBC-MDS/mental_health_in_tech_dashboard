72d2ac011bc2cbebcf8122d191bd0621f097dc2c
nickmao1994 <nickmao1994@gmail.com>
2022-03-02 09:11:31 -0800

    Update static plots

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import altair as alt
+import numpy as np
 
 import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html
@@ -15,7 +16,38 @@ app = Dash(
 server = app.server
 
 data = pd.read_csv("data/processed/survey.csv")
+data = data.drop(data[data["Q8"] == "Female"].index)
 logo = "https://cdn-icons-png.flaticon.com/512/2017/2017231.png"
+
+# data wrangling for plots
+
+# create employer size df
+size = data
+
+# create gender count and percentage df
+gender = data
+gender[gender["Gender"] == "Woman"] = "Female"
+gender[gender["Gender"] == "woman"] = "Female"
+
+gender = pd.DataFrame(gender["Gender"].value_counts())
+
+gender["Gender"] = gender["Gender"].astype(int)
+gender["Pctg"] = round((gender["Gender"] / np.sum(gender["Gender"]))*100, 2)
+gender["Count"] = gender["Gender"]
+gender["Gender"] = gender.index
+
+# create age df
+age = data.dropna()
+
+# create benefit count and percentage df
+data = data.drop(data[data["Q8"] == "Female"].index)
+benefit = pd.DataFrame(data["Q8"].value_counts())
+
+benefit["Benefit"] = benefit["Q8"].astype(int)
+benefit["Pctg"] = round((benefit["Benefit"] / np.sum(benefit["Benefit"]))*100, 2)
+benefit["Count"] = benefit["Q8"]
+benefit["Answer"] = benefit.index
+
 
 def tabs():
     tabs_layout = html.Div(
@@ -41,8 +73,50 @@ def tabs():
   
 def tab1():
     """Layout structure for Summary Overview"""
+    
+    """Test plot"""
+    
+    
+    # pie plot for gender
+    pie_gender = alt.Chart(gender).mark_arc(outerRadius = 120).encode(
+        theta = alt.Theta("Count:Q", stack = True), 
+        color = alt.Color("Gender:N")
+    )
+
+    text = pie_gender.mark_text(radius = 140, size = 20).encode(
+        text = "Pctg:Q",
+        color = "Gender")
+
+    pg = pie_gender + text
+    
+    
+    # bar plot for size
+    bar_size = alt.Chart(size).mark_bar().encode(
+        alt.X("count():Q"),
+        alt.Y("Q5:N", sort = "-x", axis=alt.Axis(title='Company Size'))
+     )
+
+    # bar plot for age
+    bar_age = alt.Chart(age).mark_bar().encode(
+        alt.X("count():Q"),
+        alt.Y("Age:N", sort = "-x")
+    )
+    
+    # pie plot for benefit
+    pie_benefit = alt.Chart(benefit).mark_arc(outerRadius = 100).encode(
+        theta = alt.Theta("Count:Q", stack = True), 
+        color = alt.Color("Answer:N")
+    )
+
+    text = pie_benefit.mark_text(radius = 140, size = 20).encode(
+        text = "Pctg:Q",
+        color = "Answer")
+
+    pb = pie_benefit + text
+
+
     summary_overview = html.Div(
-        [
+        [dbc.Row([
             dbc.Col([
                     html.H1(children="Mental Health in Tech Dashboard"), 
                     html.Br(),
@@ -56,9 +130,47 @@ def tab1():
                         html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),                
                     ]
                 )
-            ], width=2)           
+            ], width=3),
+            dbc.Col([
+                dbc.Row([
+                    dbc.Col([
+                        html.Iframe(
+                            id = 'pg',
+                            srcDoc = pg.to_html(),
+                            style={'border-width': '0', 'width': '100%', 'height': '400px'}
+                        )
+                    ]),
+                    dbc.Col([
+                        html.Iframe(
+                            id = 'bar_size',
+                            srcDoc = bar_size.to_html(),
+                            style={'border-width': '0', 'width': '100%', 'height': '400px'}
+                        )
+                    ])
+
+                ]),
+                dbc.Row([
+                    dbc.Col([
+                        html.Iframe(
+                            id = 'bar_age',
+                            srcDoc = bar_age.to_html(),
+                            style={'border-width': '0', 'width': '100%', 'height': '400px'}
+                        )
+                    ]),
+                    dbc.Col([
+                        html.Iframe(
+                            id = 'pb',
+                            srcDoc = pb.to_html(),
+                            style={'border-width': '0', 'width': '100%', 'height': '400px'}
+                        )
+                    ])
+                ])
+            ])
+        ])
         ]
     )
+    
+
     
     return summary_overview
     
