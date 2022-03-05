8472b25b65fc2dfc6de04e9807e4922dc69ebfda
Steven Lio <stevenlio88@yahoo.com.hk>
2022-03-05 12:04:01 -0800

    Update tab 3

--- src/app.py
+++ src/app.py
@@ -2,13 +2,18 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
-import dash_bootstrap_components as dbc
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
+import json
 from vega_datasets import data as dt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+
+from urllib.request import urlopen
+
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import dash
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -20,41 +25,8 @@ app = Dash(
 
 server = app.server
 
-
 logo = "https://cdn-icons-png.flaticon.com/512/2017/2017231.png"
 data = pd.read_csv("data/processed/survey.csv")
-data_ref = pd.read_csv("data/processed/survey_column.csv")
-us_state = pd.read_csv("src/assets/US_States.csv")
-
-# static sources needed for map plot
-source = dt.unemployment.url
-states = alt.topo_feature(dt.us_10m.url, 'states')
-us_state_abbrev = {
-'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
-'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
-'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
-'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
-'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
-'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK',
-'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
-'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA',
-'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
-
-state_id_dict = {'AL': 1, 'AK': 2, 'AZ': 4, 'AR': 5, 'CA': 6, 'CO': 8, 'CT': 9, 'DE': 10,
-    'DC': 11, 'FL': 12, 'GA': 13, 'HI': 15, 'ID': 16, 'IL': 17, 'IN': 18, 'IA': 19, 'KS': 20,
-    'KY': 21, 'LA': 22, 'ME': 23, 'MD': 24, 'MA': 25, 'MI': 26, 'MN': 27, 'MS': 28, 'MO': 29,
-    'MT': 30, 'NE': 31, 'NV': 32, 'NH': 33, 'NJ': 34, 'NM': 35, 'NY': 36, 'NC': 37, 'ND': 38,
-    'OH': 39, 'OK': 40, 'OR': 41, 'PA': 42, 'RI': 44, 'SC': 45, 'SD': 46, 'TN': 47, 'TX': 48,
-    'UT': 49, 'VT': 50, 'VA': 51, 'WA': 53, 'WV': 54, 'WI': 55, 'WY': 56, 'PR': 72}
-
-
-# data wrangling for map view
-surv_data = data.loc[data['Country'] == "United States"]
-surv_data.dropna(subset = ["state"], inplace=True)
-surv_data['state_id'] = surv_data['state'].map(state_id_dict).fillna(surv_data['state'])
-surv_data_grouped = surv_data.groupby(['state', 'state_id'], as_index=False)['Q3'].value_counts(normalize=True)
-surv_data_grouped = pd.DataFrame(surv_data_grouped)
-surv_data_grouped = surv_data_grouped.loc[surv_data_grouped['Q3'] == 'Yes']
 
 
 def pie_chart(df, col, colors=None, title=None):
@@ -172,7 +144,7 @@ def tab1():
                                 href="https://www.kaggle.com/osmi/mental-health-in-tech-2016",
                                 title="Data set")
                             ]),
-                    ]),
+                    ],color='lightskyblue'),
                 ], width=3,style={'margin-right': '0px', 'margin-left': '20px'}),
                 dbc.Col([            
                     dbc.Row([
@@ -205,9 +177,18 @@ def tab1():
     ])
     return layout
     
-qdict = {"Q14": "Do you think that discussing a mental health issue with your employer would have negative consequences?",
-         "Q15": "Do you think that discussing a physical health issue with your employer would have negative consequences?",
-         "Q21": "Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?"}    
+qdict = {"Q11":"Does your employer provide resources to learn more about mental health issues and how to seek help?",
+"Q12":"Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?",
+"Q13":"How easy is it for you to take medical leave for a mental health condition?",
+"Q14":"Do you think that discussing a mental health issue with your employer would have negative consequences?",
+"Q15":"Do you think that discussing a physical health issue with your employer would have negative consequences?",
+"Q16":"Would you be willing to discuss a mental health issue with your coworkers?",
+"Q17":"Would you be willing to discuss a mental health issue with your direct supervisor(s)?",
+"Q18":"Would you bring up a mental health issue with a potential employer in an interview?",
+"Q19":"Would you bring up a physical health issue with a potential employer in an interview?",
+"Q20":"Do you feel that your employer takes mental health as seriously as physical health?",
+"Q21":"Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?"}  
+        
 def tab2():
     """Layout structure for Interactive View"""
     chart_tpye = ["Bar", "Pie"]
@@ -231,13 +212,15 @@ def tab2():
                                     value = "Bar",
                                     options = [{'label': t, 'value': t} for t in chart_tpye]),
                                 html.Br(),
+                                
                                 html.H4("Survey questions"),
                                 dcc.Dropdown(
                                     id = 'q-widget',
-                                    value = 'Q14',
+                                    value = list(qdict.keys())[0],
                                     options = [{'label': q, 'value': p} for p, q in qdict.items()],
                                     optionHeight = 100),
                                 html.Br(),
+                                
                                 html.H4("Gender"),
                                 dcc.Dropdown(
                                     id = 'gender-widget',
@@ -245,6 +228,7 @@ def tab2():
                                     options = [{'label': gender, 'value': gender} for gender in genderlist],
                                     multi = True),
                                 html.Br(),
+                                
                                 html.H4("Age"),
                                 dcc.Dropdown(
                                     id = 'age-widget',
@@ -252,6 +236,7 @@ def tab2():
                                     options = [{'label': age, 'value': age} for age in agelist],
                                     multi = True),
                                 html.Br(),
+                                
                                 html.H4("Company size"),
                                 dcc.Dropdown(
                                     id = 'size-widget',
@@ -265,9 +250,8 @@ def tab2():
                     dbc.Toast([
                         html.Iframe(
                             id = 'interactive',
-                            style = {'border-width': '500', 'width': '100%', 'height': '800px'}
+                            style = {'border-width': '500', 'width': '100%', 'height': '500px'}
                         )
-                        #dcc.Graph(id="interactive"),
                         ], style={"width": "90%","height": "100%"}),
                         ],className="chart-box",style={'margin-right': '0px', 'margin-left': '0px'}),
                     ]),
@@ -276,47 +260,62 @@ def tab2():
 
     return layout
     
+    
+fnameDict = {
+    "Q11":["Yes","No","Dont' know"],
+    "Q12":["Yes","No","Dont' know"],
+    "Q13":["Very difficult","Somewhat difficult","Somewhat easy","Very easy","Dont' know"],
+    "Q14":["Yes","No","Maybe"],
+    "Q15":["Yes","No","Maybe"],
+    "Q16":["Yes","No","Some of them"],
+    "Q17":["Yes","No","Some of them"],
+    "Q18":["Yes","No","Maybe"],
+    "Q19":["Yes","No","Maybe"],
+    "Q20":["Yes","No","Dont' know"],
+    "Q21":["Yes","No"]
+}  
+
+names = list(fnameDict.keys())
+
+
 def tab3():
-    """Layout structure for Map View"""
-
-    chloropleth = alt.Chart(states).mark_geoshape().encode(
-        color='proportion:Q',
-        tooltip=['state:N', 'proportion:Q']
-    ).transform_lookup(
-        lookup='id',
-        from_=alt.LookupData(surv_data_grouped, 'state_id', ['state', 'proportion'])
-    ).project(
-        type='albersUsa'
-    ).properties(
-        width=500,
-        height=300,
-        title="Proportion of Those who have Seeked Mental Health Help by State"
-    )
-
+    """Layout structure for Interactive View"""
     layout = html.Div([
-        html.Br(),
-        dbc.Row([
-            dbc.Col([
-                dbc.Toast([
-                    html.H1(children="Mental Health in Tech Dashboard"), 
-                    html.Br(),
-                    html.P([
-                            html.H3("Controls"),
-                        ]),
-                    ])
-                ], width=3,style={'margin-right': '0px', 'margin-left': '20px'}),
+        dbc.Container([
+            html.Br(),
+            dbc.Row([
                 dbc.Col([
                     dbc.Toast([
-                            html.Iframe(
-                                id = 'chloropleth',
-                                srcDoc = chloropleth.to_html(),
-                                style={'border-width': '0', 'width': '100%', 'height': '400px'}
-                            )
-                    ])
+                        html.H1(children="Mental Health in Tech Dashboard"), 
+                        html.Br(),
+                            html.P([
+                                html.Br(),
+                                html.H4("Survey questions"),
+                                dcc.Dropdown(
+                                    id = 'map_q-widget',
+                                    value = list(qdict.keys())[0],
+                                    options = [{'label': q, 'value': p} for p, q in qdict.items()],
+                                    optionHeight = 100),
+                                html.Br(),
+                                html.H4("Response"),
+                                dcc.Dropdown(
+                                    id = 'answer-widget',
+                                    #value = list(fnameDict.values())[0],
+                                    ),
+                            ]),
+                        ]),
+                ], width=5,style={'margin-right': '0px', 'margin-left': '20px'}),
+                dbc.Col([
+                    dbc.Toast([
+                        html.Iframe(
+                            id = 'interactive_map',
+                            style = {'border-width': '500', 'width': '100%', 'height': '500px'}
+                        )
+                        ], style={"width": "90%","height": "100%"}),
+                        ],className="chart-box",style={'margin-right': '0px', 'margin-left': '0px'}),
+                    ]),
                 ])
             ])
-        ]
-    )
     
     return layout
 
@@ -349,10 +348,10 @@ app.layout = html.Div([
     ])
 ])
 
+
 @app.callback(
     Output("tabs-content", "children"), [Input("tabs", "active_tab")]
 )
-
 def select_tab(active_tab):
     if active_tab == "tab-1":
         return tab1()
@@ -361,9 +360,9 @@ def select_tab(active_tab):
     elif active_tab == "tab-3":
         return tab3()
 
-
+       
 @app.callback(
-    Output("interactive", "srcDoc"),
+    Output("interactive", component_property='srcDoc'),
     inputs = dict(question = Input('q-widget', "value"),
                   chart_type = Input('chart-widget', "value"),
                   gender = Input('gender-widget', "value"),
@@ -372,24 +371,101 @@ def select_tab(active_tab):
 )
 
 def interactive(question, chart_type, gender, size, age):
-    data = pd.read_csv("data/processed/survey.csv")   
+    qdict = {"Q11":"Does your employer provide resources to learn more about mental health issues and how to seek help?",
+    "Q12":"Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?",
+    "Q13":"How easy is it for you to take medical leave for a mental health condition?",
+    "Q14":"Do you think that discussing a mental health issue with your employer would have negative consequences?",
+    "Q15":"Do you think that discussing a physical health issue with your employer would have negative consequences?",
+    "Q16":"Would you be willing to discuss a mental health issue with your coworkers?",
+    "Q17":"Would you be willing to discuss a mental health issue with your direct supervisor(s)?",
+    "Q18":"Would you bring up a mental health issue with a potential employer in an interview?",
+    "Q19":"Would you bring up a physical health issue with a potential employer in an interview?",
+    "Q20":"Do you feel that your employer takes mental health as seriously as physical health?",
+    "Q21":"Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?"}   
+
     df_p = data[data.Gender.isin(gender) &
           data.Q5.isin(size) &
           data.Age.isin(age)]
-
-    qdict = {"Q14": "Do you think: discussing a mental health issue <br> with your employer would have <br> negative consequences?",
-         "Q15": "Do you think that discussing a physical health issue <br> with your employer would have negative consequences?",
-         "Q21": "Have you heard of or observed negative consequences <br> for coworkers with mental health <br> conditions in your workplace?"}    
-
+   
+    title = qdict[question]
+    titlesplit = title.split(" ")
+    title_n = len(titlesplit)//3
+    title = ' '.join(titlesplit[:title_n]) + " <br> " + ' '.join(titlesplit[title_n:-title_n]) + " <br> " + ' '.join(titlesplit[-title_n:])
+    
+   
     if chart_type == "Bar":
         order = ["Yes", "No", "Maybe"]
-        fig = bar_chart(df_p, question, orientation="v", title=qdict[question], order=order)
+        fig = bar_chart(df_p, question, orientation="v", title=title, order=order)
         return fig.to_html()
     
     if chart_type == "Pie":       
-        fig = pie_chart(df_p, question, colors=['skyblue','navy','lightgray'], title=qdict[question])        
+        fig = pie_chart(df_p, question, colors=['skyblue','navy','lightgray'], title=title)        
         return fig.to_html()
 
+  
+@app.callback(
+    Output('answer-widget', 'options'),
+    [Input('map_q-widget', 'value')]
+)
+def update_date_dropdown(name):
+    return [{'label': i, 'value': i} for i in fnameDict[name]]
+
+@app.callback(
+    Output("interactive_map", component_property='srcDoc'),
+    inputs = dict(map_question = Input('map_q-widget', "value"),
+                  answer = Input('answer-widget', "value"))
+)
+
+
+def interactive_map(map_question, answer):
+    us_states = pd.DataFrame({'name':['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','NewJersey','NewMexico','NewYork','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','RhodeIsland','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'],
+    'state':['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']}).set_index("state")
+
+    country = 'United States'
+    data_us = data[data.Country == country]
+
+    if type(answer) == str:
+        answer=[answer]
+
+    if answer is None:
+        answer = ["Yes","Very difficult"]
+        answer = [data_us[data_us[map_question].isin(answer)][map_question][0]]
+
+    response = pd.DataFrame(data_us[data_us[map_question].isin(answer)].groupby('state')[map_question].agg("count"))
+    response = response.rename(columns={map_question: "response"})
+    
+    base = pd.DataFrame(data_us.groupby('state')[map_question].agg("count"))
+    base = base.rename(columns={map_question: "base"})
+
+    data_map = pd.merge(
+        response,
+        base,
+        how="right",
+        left_index=True,
+        right_index=True,).fillna(0)
+
+    data_map["Percent"] = data_map['response'] / data_map['base'] * 100.0
+    data_map = pd.merge(
+        data_map,
+        us_states,
+        how="left",
+        left_index=True,
+        right_index=True)
+    
+    data_map = data_map.replace(0.0, np.NaN)
+    
+    title = qdict[map_question]
+    titlesplit = title.split(" ")
+    title_n = len(titlesplit)//2
+    title = ' '.join(titlesplit[:title_n]) + " <br> " + ' '.join(titlesplit[-title_n:])
+    title = '<b>' + title + '</b> <br> (' + ', '.join(answer) + ')'
+    
+    fig = go.Figure(data=go.Choropleth(locations=data_map.index,z = data_map['Percent'],locationmode = 'USA-states',colorscale = 'Reds',colorbar_title = "Percent",))
+    fig.update_layout(title=f'{title}', title_x=0.5)
+    fig.update_layout(margin=dict(l=20, r=20, t=100, b=50),#, width = 800, height = 800,
+                      geo_scope='usa')
+    return fig.to_html()
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
